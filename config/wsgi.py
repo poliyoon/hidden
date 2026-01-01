1872a1265d29cae55a384cc1c9a4fec0e3c4ef81
poliyoon <policeyck@gmail.com>
2026-01-01 18:03:05 +0900

    Fix: Rename application to app in wsgi.py for Vercel

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,16 +1,8 @@
-"""
-WSGI config for config project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
+# 2026-01-01 18:03
 import os
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-application = get_wsgi_application()
+app = get_wsgi_application()
